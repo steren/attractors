fc69cfc0d19c531d7418368a552783cef0bb8842
Steren <steren.giannini@gmail.com>
2015-08-02 19:37:37 +0200

    use a simple 2d canvas instead

--- ninis.js
+++ ninis.js
@@ -1,90 +1,70 @@
-var camera, scene, renderer;
+var canvas, ctx;
 
-//var geometry;
+var N = 2000;
 
 var mouseX = 0, mouseY = 0;
 
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
-var lineGeometries = [];
+var pointsX = [];
+var pointsY = [];
 
 init();
 animate();
 
 function init() {
-    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.set( 0, 0, 2000 );
+  canvas = document.getElementById("paint");
+  ctx = canvas.getContext("2d");
 
-    scene = new THREE.Scene();
 
-    var material = new THREE.LineBasicMaterial({color: 0xF7F6F5, linewidth: 1});
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-    for ( var i = 0; i < 2000; i ++ ) {
-      var geometry = new THREE.Geometry();
-      var vertex = new THREE.Vector3();
-      vertex.x = Math.random() * 4000 - 2000;
-      vertex.y = Math.random() * 4000 - 2000;
-      vertex.z = Math.random() * 4000 - 2000;
-      geometry.vertices.push(
-          vertex,
-          new THREE.Vector3( vertex.x, vertex.y, vertex.z )
-      );
-      lineGeometries.push( geometry );
 
-      var line = new THREE.Line( geometry, material );
-      scene.add( line );
-      
-    }
+  for(var i = 0; i < N; i++) {
+    pointsX.push(Math.random() * canvas.width - canvas.width / 2);
+    pointsY.push(Math.random() * canvas.height - canvas.height / 2);
+  }
 
-    camera.lookAt( scene.position );
-
-    renderer = new THREE.WebGLRenderer( { preserveDrawingBuffer: true, alpha: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.sortObjects = false;
-    renderer.autoClearColor = false;
-
-    document.body.appendChild( renderer.domElement );
-   
-
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-
-    window.addEventListener( 'resize', onWindowResize, false );
-
+  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+  window.addEventListener( 'resize', onWindowResize, false );
 }
 
 function onWindowResize() {
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
+  windowHalfX = window.innerWidth / 2;
+  windowHalfY = window.innerHeight / 2;
+    // todo resize canvas
 }
 
 function onDocumentMouseMove(event) {
-    mouseX = ( event.clientX - windowHalfX );
-    mouseY = ( event.clientY - windowHalfY );
+  mouseX = ( event.clientX - windowHalfX );
+  mouseY = ( event.clientY - windowHalfY );
 }
 
 
 function animate() {
-    requestAnimationFrame( animate );
-    render();
+  requestAnimationFrame( animate );
+  render();
 }
 
 function render() {
-  for (var i = 0; i < lineGeometries.length; i++ ) {
-    lineGeometries[i].vertices[0].x = lineGeometries[i].vertices[1].x;
-    lineGeometries[i].vertices[0].y = lineGeometries[i].vertices[1].y;
-    lineGeometries[i].vertices[0].z = lineGeometries[i].vertices[1].z;
+  for (var i = 0; i < pointsX.length; i++ ) {
+    var oldX = pointsX[i];
+    var oldY = pointsY[i];
+    var newX = oldX + mouseX * .05;
+    var newY = oldY + mouseY * .05;
+    drawline(oldX, oldY, newX, newY);
 
-    lineGeometries[i].vertices[1].x += mouseX * .05;
-    lineGeometries[i].vertices[1].y += mouseY * .05;
-
-    lineGeometries[i].verticesNeedUpdate = true;
+    pointsX[i] = newX;
+    pointsY[i] = newY;
   }
-  renderer.render( scene, camera );
+}
+
+function drawline(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.strokeStyle = "#F7F6F5";
+  ctx.moveTo(x1,y1);
+  ctx.lineTo(x2,y2);
+  ctx.stroke();
 }
