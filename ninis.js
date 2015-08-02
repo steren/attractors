f33e1fb6601849b60d9bbd2018145f9fc06ebcf2
Steren <steren.giannini@gmail.com>
2015-08-02 20:51:53 +0200

    very ugly gravity attractor

--- ninis.js
+++ ninis.js
@@ -1,14 +1,18 @@
 var canvas, ctx;
 
-var N = 2000;
+var N = 1000;
+
+var G = 100;
+var mCursor = 100
+
+var speed = 1000 / (100 * 1000 * 1000);
 
 var mouseX = 0, mouseY = 0;
 
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
-
 var pointsX = [];
 var pointsY = [];
+
+var lastTime;
 
 init();
 animate();
@@ -23,8 +27,8 @@ function init() {
 
 
   for(var i = 0; i < N; i++) {
-    pointsX.push(Math.random() * canvas.width - canvas.width / 2);
-    pointsY.push(Math.random() * canvas.height - canvas.height / 2);
+    pointsX.push(Math.random() * canvas.width);
+    pointsY.push(Math.random() * canvas.height);
   }
 
   document.addEventListener( 'mousemove', onDocumentMouseMove, false );
@@ -32,32 +36,32 @@ function init() {
 }
 
 function onWindowResize() {
-  windowHalfX = window.innerWidth / 2;
-  windowHalfY = window.innerHeight / 2;
-    // todo resize canvas
+  // todo resize canvas
 }
 
 function onDocumentMouseMove(event) {
-  mouseX = ( event.clientX - windowHalfX );
-  mouseY = ( event.clientY - windowHalfY );
+  mouseX = event.clientX
+  mouseY = event.clientY;
 }
 
 
-function animate() {
+function animate(timestamp) {
   requestAnimationFrame( animate );
-  render();
+  render(timestamp);
 }
 
-function render() {
+function render(timestamp) {
+  if (!lastTime) { lastTime = timestamp; }
+  var delta = timestamp - lastTime;
+
   for (var i = 0; i < pointsX.length; i++ ) {
     var oldX = pointsX[i];
     var oldY = pointsY[i];
-    var newX = oldX + mouseX * .05;
-    var newY = oldY + mouseY * .05;
-    drawline(oldX, oldY, newX, newY);
+    var newPosition = getNewPosition(oldX, oldY, delta);
+    drawline(oldX, oldY, newPosition[0], newPosition[1]);
 
-    pointsX[i] = newX;
-    pointsY[i] = newY;
+    pointsX[i] = newPosition[0];
+    pointsY[i] = newPosition[1];
   }
 }
 
@@ -67,4 +71,23 @@ function drawline(x1, y1, x2, y2) {
   ctx.moveTo(x1,y1);
   ctx.lineTo(x2,y2);
   ctx.stroke();
+}
+
+function getNewPosition(x, y, delta) {
+  if(delta > 0) {
+    var r2 = (x - mouseX) * (x - mouseX) + (y - mouseY) * (y - mouseY); 
+
+    var newX, newY;
+    
+    if(r2 > G) {
+      newX = x - G * mCursor * (x - mouseX) / (r2 * Math.sqrt(r2));
+      newY = y - G * mCursor * (y - mouseY) / (r2 * Math.sqrt(r2));
+    } else {
+      newX = x;
+      newY = y;
+    }
+    return [newX, newY];
+  } else {
+    return [x, y];
+  }
 }
